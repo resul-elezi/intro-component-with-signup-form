29e5173875c661ded41055142dfd33c3968a8502
Resul Elezi <resul_elezi@protonmail.com>
2023-09-06 13:01:57 +0200

    #

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ const LAST_NAME = document.getElementById("last-name");
 const E_MAIL = document.getElementById("e-mail");
 const PASS_W = document.getElementById("password");
 const SUBMIT = document.getElementById("submit");
+
+FORM.addEventListener("submit", e => {
+	e.preventDefault();
+	
+	let firstName = FIRST_NAME.value.trim();
+	let lastName = LAST_NAME.value.trim();
+	let eMail = E_MAIL.value.trim();
+	let passWord = PASS_W.value.trim();
+	let passValPat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+	if(firstName === ""){
+		errorFunc(FIRST_NAME, "First Name cannot be empty")
+	}
+
+	if(lastName === ""){
+		errorFunc(LAST_NAME, "Last Name cannot be empty")
+	}
+
+	if(eMail === ""){
+        errorFunc(E_MAIL, "E-Mail cannot be empty")
+    } else if(eMail !== '' && !eMail.match(passValPat)){
+        errorFunc(E_MAIL, "Looks like this is not an email")
+    }
+
+	if(passWord === ""){
+		errorFunc(PASS_W, "Password cannot be empty")
+	}
+})
+
+function errorFunc(req, message){
+	const FORM_CONTROL = req.parentElement;
+	const SPAN = FORM_CONTROL.querySelector("span");
+	SPAN.innerText = message;
+	req.className += "error-icon";
+	SPAN.className += "error-text";
+	if(req !== E_MAIL){
+	   req.value = "";
+	} {
+        req.style.color = "hsl(0, 100%, 74%)";
+     } 
+}
