8f6faf4bd37150187be5763d130e5656b9a6235e
Resul Elezi <resul_elezi@protonmail.com>
2023-09-10 17:09:46 +0200

    added js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+"use strict";
 
+const VALITDATE_FORM = formSelector => {
+    const FORM_ELEMENT = document.querySelector(formSelector);
+
+        const VALIDATE_OPTIONS = [
+
+        ]
+
+        const VALIDATE_SINGLE_FORM_GROUP = formGroup => {
+            const INPUT = formGroup.querySelector("input");
+            const ERROR_CONTAINER = formGroup.querySelector(".error-text");
+            const ERROR_ICON = formGroup.querySelector(".error-icon");
+        }
+
+    FORM_ELEMENT.setAttribute("novalidate", "");
+
+
+    FORM_ELEMENT.addEventListener("submit", event => {
+        event.preventDefault();
+        VALIDATE_ALL_FORM_GROUPS(FORM_ELEMENT);
+    });
+
+    const VALIDATE_ALL_FORM_GROUPS = formToValidate => {
+        const FORM_GROUPS = Array.from(formToValidate.querySelectorAll(".form-group"));
+
+        FORM_GROUPS.forEach(formGroup => {
+            VALIDATE_SINGLE_FORM_GROUP(formGroup);
+        });
+    }
+};
+
+VALIDATE_FORM("#form");
