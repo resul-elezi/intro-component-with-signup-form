be3f6908b88f479b152748c4e340af2bf0c73f48
Resul Elezi <resul_elezi@protonmail.com>
2023-09-10 20:08:20 +0200

    customized js

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 "use strict";
 
-const VALITDATE_FORM = formSelector => {
+const VALIDATE_FORM = formSelector => {
     const FORM_ELEMENT = document.querySelector(formSelector);
 
         const VALIDATE_OPTIONS = [
-
-        ]
-
+            {
+                attribute: "pattern",
+                isValid: INPUT => {
+                    const PATTERN_REGEX = new RegExp(INPUT.pattern);
+                    return PATTERN_REGEX.test(INPUT.value);
+                },
+                errorMessage: (INPUT) => `Looks like this is not an ${INPUT.placeholder}`,
+            },
+            {
+                attribute: "required",
+                isValid: INPUT => INPUT.value.trim() !== "",
+                errorMessage: (INPUT) => `${INPUT.placeholder} cannot be empty`
+            },
+        ];
+    
         const VALIDATE_SINGLE_FORM_GROUP = formGroup => {
             const INPUT = formGroup.querySelector("input");
             const ERROR_CONTAINER = formGroup.querySelector(".error-text");
             const ERROR_ICON = formGroup.querySelector(".error-icon");
-        }
+
+            let formGroupError = false;
+            for(const OPTION of VALIDATE_OPTIONS) {
+                if(INPUT.hasAttribute(OPTION.attribute) && !OPTION.isValid(INPUT)) {
+                    ERROR_CONTAINER.textContent = OPTION.errorMessage(INPUT);
+                    ERROR_ICON.classList.remove("hidden");
+                    ERROR_CONTAINER.classList.remove("hidden");
+                    formGroupError = true;
+                }
+            }
+            if(!formGroupError) {
+                // ERROR_CONTAINER.textContent = "";
+                ERROR_ICON.classList.add("hidden");
+                ERROR_CONTAINER.classList.add("hidden");
+            }
+        };
 
     FORM_ELEMENT.setAttribute("novalidate", "");
 
