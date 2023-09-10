ce2f9f88e0a0e4a9e0a85fedf7e7dcc8e43919be
Resul Elezi <resul_elezi@protonmail.com>
2023-09-10 15:44:55 +0200

    #

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-'use strict';
-
-const FORM = document.getElementById("form");
-const FIRST_NAME = document.getElementById("first-name");
-const LAST_NAME = document.getElementById("last-name");
-const E_MAIL = document.getElementById("e-mail");
-const PASS_W = document.getElementById("password");
-const INPUT = document.querySelectorAll("input");
-
 
